51c001330aa52ec3f4faea2d84770672e23e1b02
themoutaouakil <moutaouakilayoub8@gmail.com>
2026-01-24 17:05:47 +0100

    Revert "Merge feat/sign-up-page: Move auth services to feature infrastructure and implement Clean Architecture"
    This reverts commit b5c8d20aae0bc2440ba9dde56c90c67569b7a10e, reversing
    changes made to e31fb7b51b0461947241830cef35ed9318a1e6c5.

--- lib/feature/auth/domain/repositories/auth_repository.dart
+++ lib/feature/auth/domain/repositories/auth_repository.dart
@@ -12,21 +12,4 @@ abstract class AuthRepository {
   Future<Result<Unit>> signOut();
 
   Stream<Result<AuthUser?>> watchAuthState();
-
-  /// Create a new user account with email and password
-  Future<Result<AuthUser>> createUserWithEmailAndPassword({
-    required EmailAddress email,
-    required Password password,
-  });
-
-  /// Send phone verification code and return verification ID
-  Future<Result<String>> sendPhoneVerification({
-    required String phoneNumber,
-  });
-
-  /// Link phone credential to the current authenticated user
-  Future<Result<Unit>> linkPhoneCredential({
-    required String verificationId,
-    required String smsCode,
-  });
 }
